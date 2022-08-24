efb4002c8030496a9e04a81a24717772d88453eb
Kris Marshall <rflctnofu@hotmail.com>
2022-08-24 12:42:54 -0400

    fixed image loading for portfolio/projects cards

--- portfolio/src/projects/ProjectData.jsx
+++ portfolio/src/projects/ProjectData.jsx
@@ -2,7 +2,7 @@ const ProjectData = [
     {
         id: 1,
         title: 'Game Getter',
-        image: 'https://github.com/StevenBolc/GameGetter/blob/main/public/assets/images/homepage-screenshot.png',
+        image: 'images/GameGetter.png',
         description: 'Game Getter is an interactive application that allows users to search for over 16,700 video games from various genres, publishers, release years and more! After searching, a display of all related search results will be curated just for you.',
         live: 'https://game-getter-project-2.herokuapp.com/',
         github: 'https://github.com/StevenBolc/GameGetter'
@@ -10,7 +10,7 @@ const ProjectData = [
     {
         id: 2,
         title: 'The Tech Blog',
-        image: '../assets/images/TechBlog.png',
+        image: 'images/TechBlog.png',
         description: 'The Tech Blog is a CMS style blog site for users to log in and make posts detailing thier thoughts on the tech world. While logged in, users can comment on existing posts, and a personal dashboard keeps track of their posts, that they can edit or delete.',
         live: 'https://nameless-mountain-71362.herokuapp.com/',
         github: 'https://github.com/RflctnOfU/TechBlog'
@@ -18,7 +18,7 @@ const ProjectData = [
     {
         id: 3,
         title: 'BooksVsMovies',
-        image: '../assets/images/BooksVsMovies.png',
+        image: 'images/BooksVsMovies.png',
         description: 'BooksVsMovies is a web application that allows the user to search for the title of a book or a movie and compares the two titles side by side. The user then is allowed to leave their mark on the page by giving the titles a thumbs-up, if they enjoy the title, or a thumbs-down, if they disliked the title.',
         live: 'https://rflctnofu.github.io/BooksVsMovies/',
         github: 'https://github.com/RflctnOfU/BooksVsMovies'
@@ -26,7 +26,7 @@ const ProjectData = [
     {
         id: 4,
         title: 'Note Taker',
-        image: '../assets/images/NoteTaker.png',
+        image: 'images/NoteTaker.png',
         description: 'This is an app deployed on Heroku that stores notes that are entered on the page in a database.json and displays the title of the notes that can be recalled and posted on the main part of the page. They can also be deleted. App uses express.js',
         live: 'https://lit-mountain-01965.herokuapp.com/',
         github: 'https://github.com/RflctnOfU/NoteTaker'
@@ -34,7 +34,7 @@ const ProjectData = [
     {
         id: 5,
         title: 'Weather Dashboard',
-        image: '../assets/images/WeatherDashboard.png',
+        image: 'images/WeatherDashboard.png',
         description: 'A city search app using openweather api to get current and forecast weather data. app uses fetch and features dynamic displays.',
         live: 'https://rflctnofu.github.io/WeatherDashboard/',
         github: 'https://github.com/RflctnOfU/WeatherDashboard'
@@ -42,7 +42,7 @@ const ProjectData = [
     {
         id: 6,
         title: 'Code Quiz',
-        image: '../assets/images/CodeQuiz.png',
+        image: 'images/CodeQuiz.png',
         description: 'Code Quiz is a dynamic Coding quiz making use of DOM manipulation, local storage, and timer function.',
         live: 'https://rflctnofu.github.io/codeQuiz/',
         github: 'https://github.com/RflctnOfU/codeQuiz'
